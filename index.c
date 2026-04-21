b546d5916dc8a55c8ed3b75e23226e791c932402
parnika-265 <parnika2605@gmail.com>
2026-04-21 10:45:06 +0530

    Phase 3: index_add implemented

--- index.c
+++ index.c
@@ -1,6 +1,7 @@
 // index.c — Staging area implementation
 //
 //phase 3 step 2
+//phase e step 3
 // Text format of .pes/index (one entry per line, sorted by path):
 //
 //   <mode-octal> <64-char-hex-hash> <mtime-seconds> <size> <path>
